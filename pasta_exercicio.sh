72983589f87e5da71dbcc6d8082c35bf696f25c0
reeh-asp <resilva1@live.com>
2024-08-13 20:35:27 -0300

    Update pasta_exercicio.sh

--- pasta_exercicio.sh
+++ pasta_exercicio.sh
@@ -7,6 +7,6 @@ mkdir /home/reeh/exercicios/power_shell
 echo "script finalizado"
 
 ## Para executar o documento, basta salvá-lo em seu diretorio de preferencia, após isso entre no Ubuntu e digite o comando ./pasta_exercicio.sh, para executá-lo.
-Caso o comando não funcione verificar como estão as permissões do arquivo para identificar se está execultavel ou não.
-Se não estiver como executavel utilizar o comando chmod +x pasta_exercicio.sh para torná-lo executavel.
-Após isso digitar novamente o comando ./pasta_exercicio.sh 
+## Caso o comando não funcione verificar como estão as permissões do arquivo para identificar se está execultavel ou não.
+## Se não estiver como executavel utilizar o comando chmod +x pasta_exercicio.sh para torná-lo executavel.
+## Após isso digitar novamente o comando ./pasta_exercicio.sh 
