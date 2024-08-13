f4beb9a81385c177f0be015a0fb387df0dbe32a9
reeh-asp <resilva1@live.com>
2024-08-13 19:54:04 -0300

    Create tabuada_idade.py
    create file tabuada da idade

---
+++ tabuada_idade.py
@@ -0,0 +1,27 @@
+#TABUADA 1 AO 20 COM A IDADE
+
+idade = int(input('Qual é a sua idade? '))
+
+print('A tabuada da sua idade do 1 ao 20 é:')
+
+print('1 x',(idade),'=',1*idade)
+print('2 x',idade,'=',2*idade)
+print('3 x',idade,'=',3*idade)
+print('4 x',idade,'=',4*idade)
+print('5 x',idade,'=',5*idade)
+print('6 x',idade,'=',6*idade)
+print('7 x',idade,'=',7*idade)
+print('8 x',idade,'=',8*idade)
+print('9 x',idade,'=',9*idade)
+print('10 x',idade,'=',10*idade)
+print('11 x',idade,'=',11*idade)
+print('12 x',idade,'=',12*idade)
+print('13 x',idade,'=',13*idade)
+print('14 x',idade,'=',14*idade)
+print('15 x',idade,'=',15*idade)
+print('16 x',idade,'=',16*idade)
+print('17 x',idade,'=',17*idade)
+print('18 x',idade,'=',18*idade)
+print('19 x',idade,'=',19*idade)
+print('20 x',idade,'=',20*idade)
+
